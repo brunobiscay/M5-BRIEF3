ec82e96a20d329fb1847c5d4f3075f842521cd49
Bruno BISCAY <bbiscay@mip.company>
2026-05-29 17:42:13 +0200

    Rajout endpoint /metrics pour Prometheus

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from loguru import logger
 from modules.calcul import calcul_carre
+from prometheus_fastapi_instrumentator import Instrumentator
 from pydantic import BaseModel
+from prometheus_client import Counter
+
+calcul_counter = Counter(
+    "calcul_requests_total",
+    "Nombre total d'appels à /calcul"
+)
 
 # ✅ Configuration logs
 logger.add(
@@ -9,6 +16,9 @@ logger.add(
 )
 
 app = FastAPI()
+
+# ✅ Instrumentation Prometheus
+Instrumentator().instrument(app).expose(app, endpoint="/metrics"))
 
 
 class Nombre(BaseModel):
@@ -30,6 +40,7 @@ def health():
 @app.post("/calcul")
 def calcul(nombre: Nombre):
     try:
+        calcul_counter.inc()
         logger.info(f"Requête reçue: {nombre.valeur}")
         resultat = calcul_carre(nombre.valeur)
         return {"resultat": resultat}
